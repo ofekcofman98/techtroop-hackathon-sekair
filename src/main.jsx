214824044cea82848860ee8fa3246d4bf54d3452
Ofek Cofman <ofekcofman98@gmail.com>
2026-07-14 13:36:34 +0300

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <StrictMode> 
+    <HashRouter> 
+      <App />
+    </HashRouter> 
+  </StrictMode>
+);
